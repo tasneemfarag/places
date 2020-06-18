422fd6e1ed591e94c403a5882aa8fb44b3590781
Tasneem Farag <tasneemfarag@Tasneems-MacBook-Pro-2.local>
2020-06-18 09:48:27 -0400

    adding more animation

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
-    $(".sections").hide();
-    $("#myButton").click(function () {
-        $(".sections").show();
-        $( ".even.section" ).animate({
-            marginLeft: 0
-          }, 1500 );
+  $(".sections").hide();
+  $("#myButton").click(function () {
+    $(".sections").show();
+    $("#myButton").hide();
+    $('html, body').animate({
+      scrollTop: $(".sections").offset().top
+    }, 1000)
+    $(".even.section").animate({
+      marginLeft: 0
+    }, 1500);
 
-          $( ".odd.section" ).animate({
-            marginRight: 0
-          }, 1500 );
-    });
+    $(".odd.section").animate({
+      marginRight: 0
+    }, 1500);
+  });
 });
 
