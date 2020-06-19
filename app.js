380039020ca52aba0a3b631f30cef91e03dba856
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-06-18 20:12:37 -0400

    weather card flip

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ $(document).ready(function () {
     }, 1500, function () {
       $(".section").css("background-color","#C0C0C0;");
     });
-    
+   
   });
+
+  $("#card").click(function(){
+    var cardBack = $("#card .face.back");    
+    var cardFront = $("#card .face.front");
+    
+    cardBack.addClass("front");
+    cardBack.removeClass("back");
+
+    cardFront.addClass("back");
+    cardFront.removeClass("front");
+  });
+
 });
 
