0c773cd420b425f7a821fd4c82db595ac9eda69b
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-06-19 00:19:53 -0400

    add styling, API call, callback function

---
+++ README.md
@@ -0,0 +1,13 @@
+# My Adventure
+
+My adventure project.
+
+## Documentation
+
+City Ids obtained from:
+https://openweathermap.org/current#cityid
+
+List of city ID city.list.json.gz can be downloaded here http://bulk.openweathermap.org/sample/
+
+To create your own key, go to:
+https://home.openweathermap.org/api_keys
--- app.js
+++ app.js
@@ -12,21 +12,49 @@ $(document).ready(function () {
     $(".odd.section").animate({
       marginRight: 0
     }, 1500, function () {
-      $(".section").css("background-color","#C0C0C0;");
+      $(".section").css("background-color", "#d0d0d0");
     });
-   
+
   });
 
-  $("#card").click(function(){
-    var cardBack = $("#card .face.back");    
-    var cardFront = $("#card .face.front");
-    
+  $(".card").click(function () {
+    var cardBack = $(this).find(".face.back");
+    var cardFront = $(this).find(".face.front");
+
     cardBack.addClass("front");
     cardBack.removeClass("back");
 
     cardFront.addClass("back");
     cardFront.removeClass("front");
+
   });
 
-});
 
+  var baliCityId = 550671;
+  weatherBalloon(baliCityId, setWeather, "#bali");
+
+  var cairoCityId = 360630;
+  weatherBalloon(cairoCityId, setWeather, "#cairo");
+
+  var londonCityId = 1006984;
+  weatherBalloon(londonCityId, setWeather, "#london");
+
+  var caicosCityId = 3576916;
+  weatherBalloon(caicosCityId, setWeather, "#caicos");
+
+  function weatherBalloon(cityId, setWeather, idValue) {
+    var appId = '1e2c5c69bcb9a5024c88c8d0bb7d6e8a'; // Check README to create your own key
+    fetch('https://api.openweathermap.org/data/2.5/weather?units=imperial&id=' + cityId + '&appid=' + appId)
+      .then(function (resp) { return resp.json() }) // Convert data to json
+      .then(function (data) {
+        setWeather(data.main.temp, idValue);
+      })
+      .catch(function () {
+        // catch any errors
+      });
+  }
+
+  function setWeather(temperature, idValue) {
+    $(idValue).html(temperature);
+  }
+});
